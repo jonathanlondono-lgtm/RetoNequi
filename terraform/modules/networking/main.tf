8d5b3561f6ed4e1e33e0fde5a04fcb726bcab1b4
Jonathan Londoño <jonathan.londono@pragma.com.co>
2026-07-22 08:46:04 -0500

    fix: add security group to ALB

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -120,3 +120,23 @@ resource "aws_security_group" "rds" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "alb" {
+  name        = "nequi-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -17,3 +17,7 @@ output "ecs_security_group_id" {
 output "rds_security_group_id" {
   value = aws_security_group.rds.id
 }
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
+}
